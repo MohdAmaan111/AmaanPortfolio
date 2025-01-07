df159c242aad8aff91dbc526abcb9cca5f6ba0bf
Amaan <amaanreigns01@gmail.com>
2025-01-07 12:50:17 +0530

    contact information updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,3 +169,71 @@ let rotateText = () => {
 
 rotateText();
 setInterval(rotateText, 4000);
+
+// Email submit script
+
+const form = document.getElementById("sendMessage");
+const result = document.getElementById("result");
+
+document.getElementById("sendMessage").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  // result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        // result.innerHTML = json.message;
+
+        // SweetAlert success message
+        swal({
+          title: "Message Sent Successfully!",
+          icon: "success",
+          button: "OK",
+        }).then(() => {
+          // Reload the page after the alert is closed
+          window.location.reload();
+        });
+      } else {
+        // console.log(response);
+        // result.innerHTML = json.message;
+        swal({
+          title: "Something went wrong!",
+          icon: "warning",
+          button: "OK",
+        }).then(() => {
+          // Reload the page after the alert is closed
+          window.location.reload();
+        });
+      }
+    })
+    .catch((error) => {
+      // console.log(error);
+      // result.innerHTML = "Something went wrong!";
+      swal({
+        title: "Something went wrong!",
+        icon: "warning",
+        button: "OK",
+      }).then(() => {
+        // Reload the page after the alert is closed
+        window.location.reload();
+      });
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
